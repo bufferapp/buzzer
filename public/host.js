e7d6310ef560057ffd661e25b2f8632823f353ac
Smokynight <dmitrijs.shahlovs@gmail.com>
2020-03-21 18:32:50 +0200

    Removing team concept - not needed for our app.

--- public/host.js
+++ public/host.js
@@ -11,9 +11,9 @@ socket.on('buzzes', (buzzes) => {
   buzzList.innerHTML = buzzes
     .map(buzz => {
       const p = buzz.split('-')
-      return { name: p[0], team: p[1] }
+      return { name: p[0]}
     })
-    .map(user => `<li>${user.name} on Team ${user.team}</li>`)
+    .map(user => `<li>${user.name}</li>`)
     .join('')
 })
 
--- public/join.js
+++ public/join.js
@@ -12,7 +12,6 @@ const getUserInfo = () => {
   user = JSON.parse(localStorage.getItem('user')) || {}
   if (user.name) {
     form.querySelector('[name=name]').value = user.name
-    form.querySelector('[name=team]').value = user.team
   }
 }
 const saveUserInfo = () => {
@@ -22,13 +21,12 @@ const saveUserInfo = () => {
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   user.name = form.querySelector('[name=name]').value
-  user.team = form.querySelector('[name=team]').value
   if (!user.id) {
     user.id = Math.floor(Math.random() * new Date())
   }
   socket.emit('join', user)
   saveUserInfo()
-  joinedInfo.innerText = `${user.name} on Team ${user.team}`
+  joinedInfo.innerText = `${user.name}`
   form.classList.add('hidden')
   joined.classList.remove('hidden')
   body.classList.add('buzzer-mode')
