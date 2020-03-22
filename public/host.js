619dd42e8c6d4a7354649c52836346be3af9d97a
Smokynight <dmitrijs.shahlovs@gmail.com>
2020-03-22 03:10:11 +0200

    POC on updating table in the runtime.

--- public/host.js
+++ public/host.js
@@ -1,10 +1,12 @@
 const socket = io()
 const active = document.querySelector('.js-active')
 const buzzList = document.querySelector('.js-buzzes')
+const playerList = document.querySelector('.js-players')
 const clear = document.querySelector('.js-clear')
 
 socket.on('active', (numberActive) => {
   active.innerText = `${numberActive} joined`
+    playerList.innerHTML="<tr><th>Firstname</th></tr>"
 })
 
 socket.on('buzzes', (buzzes) => {
